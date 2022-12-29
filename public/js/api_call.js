1c00ff3856c80c9e52132d3f12f69638ec5d8722
Jacob Briggs <jdbriggs81@gmail.com>
2022-12-29 14:37:41 -0600

    merging changes from origin

--- public/js/api_call.js
+++ public/js/api_call.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
             checkLogin()
         }
     });
-    
+
     const checkLogin=()=>{
         const isLoged=localStorage.getItem("isLoged");
         isLoged==="true"&&$('#login_link').addClass('d-none');
@@ -25,21 +25,19 @@ $(document).ready(function(){
     }
     checkLogin()
 
-//register api call
-    $('#register_user').on("click",
-    function(event){
+  //register api call
+  $("#register_user").on("click", function (event) {
     event.preventDefault();
-    $('#register_error').addClass('d-none');
-    const first_name=$('#first_name').val()
-    const last_name=$('#last_name').val()
-    const email=$('#email').val()
-    const password=$('#password').val()
-    const verify_password=$('#verify_password').val()
-    if(password!==verify_password)
-    {
-        $('#register_error').removeClass('d-none')
-        $('#register_error').text("password must match")
-        return
+    $("#register_error").addClass("d-none");
+    const first_name = $("#first_name").val();
+    const last_name = $("#last_name").val();
+    const email = $("#email").val();
+    const password = $("#password").val();
+    const verify_password = $("#verify_password").val();
+    if (password !== verify_password) {
+      $("#register_error").removeClass("d-none");
+      $("#register_error").text("password must match");
+      return;
     }
     $('#register_text').addClass('d-none');
     $('#register_text_spin').removeClass('d-none');
@@ -60,7 +58,7 @@ $(document).ready(function(){
           }
       });
   })
-  
+
 //login api call
     $('#login_user').on("click",
     function(event){
@@ -89,8 +87,8 @@ $(document).ready(function(){
       });
   })
 
-//logout
-  $('#logout_link').on("click",function(){
+  //logout
+  $("#logout_link").on("click", function () {
     $.ajax({
         url:`${base_url}/logout`,
         method:"GET",
@@ -100,18 +98,22 @@ $(document).ready(function(){
           location.href = "/login"
         },
     });
-  })
-//forgot password
-$('#forgot_password').on("click",function(event){
+  });
+  //forgot password
+  $("#forgot_password").on("click", function (event) {
     event.preventDefault();
-    const email=$('#resetPasswordEmail').val()
-    $('#invalid_forgotpassword_email').addClass('d-none')
-    const isEmailValid=ValidateEmail(email)
-    if(!email||!isEmailValid){
-        !email&&$('#invalid_forgotpassword_email').removeClass('d-none')
-        !isEmailValid&&email&&$('#invalid_forgotpassword_email').removeClass('d-none')
-        !isEmailValid&&email&&$('#invalid_forgotpassword_email').text('Invalid Email')
-        return
+    const email = $("#resetPasswordEmail").val();
+    $("#invalid_forgotpassword_email").addClass("d-none");
+    const isEmailValid = ValidateEmail(email);
+    if (!email || !isEmailValid) {
+      !email && $("#invalid_forgotpassword_email").removeClass("d-none");
+      !isEmailValid &&
+        email &&
+        $("#invalid_forgotpassword_email").removeClass("d-none");
+      !isEmailValid &&
+        email &&
+        $("#invalid_forgotpassword_email").text("Invalid Email");
+      return;
     }
     $('#resetpassword_text_spin').removeClass('d-none');
     $('#resetpassword_text').addClass('d-none');
@@ -127,77 +129,72 @@ $('#forgot_password').on("click",function(event){
         $('#forgot_message').text(data.message)
         $('#forgot_message').removeClass('d-none');
       },
-      error:function(data){
-        $('#resetpassword_text').removeClass('d-none');
-        $('#resetpassword_text_spin').addClass('d-none');
-      }
+    });
   });
-})
-//reset password
-$('#reset_button').on("click",function(event){
-  event.preventDefault();
-  console.log('reset')
-  const newPassword=$('#npassword').val()
-  const confirmNewPassword=$('#cpassword').val()
-  $('#reset_error').addClass('d-none')
-  if(!newPassword||!confirmNewPassword){
-    $('#reset_error').removeClass('d-none')
-    $('#reset_error').text('please fill all field')
-    return
-  }
-  if(newPassword!==confirmNewPassword){
-      $('#reset_error').removeClass('d-none')
-      $('#reset_error').text('password not match')
-      return
-  }
-  $('#reset_button').val("Loading...")
-$.ajax({
-    url:window.location.href,
-    method:"POST",
-    data:{password:newPassword},
-    success:function(data)
-    {
-      $('#resetpassword_text').removeClass('d-none');
-      $('#resetpassword_text_spin').addClass('d-none');
-      $('#forgot_message').text(data.message)
-      $('#forgot_message').removeClass('d-none');
-      $('#reset_button').val("Submit")
-      location.href = "/login"
-    },
-    error:function(data){
-      $('#reset_error').removeClass('d-none')
-      $('#reset_error').text("invalid or expired link, please try again")
-      $('#resetpassword_text').removeClass('d-none');
-      $('#resetpassword_text_spin').addClass('d-none');
-      $('#reset_button').val("Submit")
-    }
-});
-})
-  //contact form
-  $('#submit_contact_form').on("click",function(event){
+  //reset password
+  $("#reset_button").on("click", function (event) {
     event.preventDefault();
-    $('#contact_form_email')
-    const name=$('#contact_form_name').val()
-    const email=$('#contact_form_email').val()
-    const message=$('#contact_form_message').val()
-    const phone=$('#contact_form_phone').val()
-    const subject=$('#contact_form_subject').val()
-
-    $('#invalid_email').addClass('d-none')
-    $('#invalid_subject').addClass('d-none')
-    $('#invalid_message').addClass('d-none')
-    console.log(subject)
-    const isEmailValid=ValidateEmail(email)
-    if(!email||!subject||!message||!isEmailValid){
-        !email&&$('#invalid_email').removeClass('d-none')
-        !isEmailValid&&email&&$('#invalid_email').removeClass('d-none')
-        !isEmailValid&&email&&$('#invalid_email').text('Invalid Email')
-        !subject&&$('#invalid_subject').removeClass('d-none')
-        !message&&$('#invalid_message').removeClass('d-none')
-        return
+    console.log("reset");
+    const newPassword = $("#npassword").val();
+    const confirmNewPassword = $("#cpassword").val();
+    $("#reset_error").addClass("d-none");
+    if (!newPassword || !confirmNewPassword) {
+      $("#reset_error").removeClass("d-none");
+      $("#reset_error").text("please fill all field");
+      return;
     }
-    $('#contact_text_spin').removeClass('d-none');
-    $('#contact_text').addClass('d-none');
+    if (newPassword !== confirmNewPassword) {
+      $("#reset_error").removeClass("d-none");
+      $("#reset_error").text("password not match");
+      return;
+    }
+    $("#reset_button").val("Loading...");
+    $.ajax({
+      url: window.location.href,
+      method: "POST",
+      data: { password: newPassword },
+      success: function (data) {
+        $("#resetpassword_text").removeClass("d-none");
+        $("#resetpassword_text_spin").addClass("d-none");
+        $("#forgot_message").text(data.message);
+        $("#forgot_message").removeClass("d-none");
+        $("#reset_button").val("Submit");
+        location.href = "/login";
+      },
+      error: function (data) {
+        $("#reset_error").removeClass("d-none");
+        $("#reset_error").text("invalid or expired link, please try again");
+        $("#resetpassword_text").removeClass("d-none");
+        $("#resetpassword_text_spin").addClass("d-none");
+        $("#reset_button").val("Submit");
+      },
+    });
+  });
+  //contact form
+  $("#submit_contact_form").on("click", function (event) {
+    event.preventDefault();
+    $("#contact_form_email");
+    const name = $("#contact_form_name").val();
+    const email = $("#contact_form_email").val();
+    const message = $("#contact_form_message").val();
+    const phone = $("#contact_form_phone").val();
+    const subject = $("#contact_form_subject").val();
+
+    $("#invalid_email").addClass("d-none");
+    $("#invalid_subject").addClass("d-none");
+    $("#invalid_message").addClass("d-none");
+    console.log(subject);
+    const isEmailValid = ValidateEmail(email);
+    if (!email || !subject || !message || !isEmailValid) {
+      !email && $("#invalid_email").removeClass("d-none");
+      !isEmailValid && email && $("#invalid_email").removeClass("d-none");
+      !isEmailValid && email && $("#invalid_email").text("Invalid Email");
+      !subject && $("#invalid_subject").removeClass("d-none");
+      !message && $("#invalid_message").removeClass("d-none");
+      return;
+    }
+    $("#contact_text_spin").removeClass("d-none");
+    $("#contact_text").addClass("d-none");
     $.ajax({
         url:`${base_url}/contactform`,
         method:"POST",
@@ -214,29 +211,29 @@ $.ajax({
             $('#contact_text_spin').addClass('d-none');
           }
     });
-  })
-function ValidateEmail(email)
-{
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-if(email?.match(mailformat)){
-    return true;
-}
-else{
-return false;
-}
-}
-//check google auth if not exist
-const urlParams = new URLSearchParams(window.location.search);
-const myParam = urlParams.get('error');
-if(myParam == "Google-Auth-Not-Exist") {
-    $('#login_error').removeClass('d-none')
-    $('#login_error').text("This account not associated with google please use your email and password to login")
+  });
+  function ValidateEmail(email) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email?.match(mailformat)) {
+      return true;
+    } else {
+      return false;
+    }
   }
-if(myParam == "No-Auth-Redirect") {
-    location.href = "/login"
-}
-//get all selected product for order
-//on order complete buton click
+  //check google auth if not exist
+  const urlParams = new URLSearchParams(window.location.search);
+  const myParam = urlParams.get("error");
+  if (myParam == "Google-Auth-Not-Exist") {
+    $("#login_error").removeClass("d-none");
+    $("#login_error").text(
+      "This account not associated with google please use your email and password to login"
+    );
+  }
+  if (myParam == "No-Auth-Redirect") {
+    location.href = "/login";
+  }
+  //get all selected product for order
+  //on order complete buton click
 
 // $('#pp-id').on('click',function(){
 //   const product_ordered=[]
@@ -255,7 +252,6 @@ $('input[id="telehealth-appt-checkbox"]').on("click",function () {
   $('#cart-total-price').text(total_price)
 
 });
-  
+
 
 });
-  
